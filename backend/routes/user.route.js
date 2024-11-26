934159fc32b22fed748c484fe79af4d880b8628a
mahdiisabry1 <mahdiisabry@gmail.com>
2024-11-26 14:57:52 +0530

    backend error

--- backend/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/anothertest", (req, res) => {
-  res.status(200).send("This is a user router test");
-});
-
-export default router;
