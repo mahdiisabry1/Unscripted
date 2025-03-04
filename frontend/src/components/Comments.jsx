e2f6c6f740d515fb4d28f3f1da08bae09f3ed513
mahdiisabry1 <mahdiisabry@gmail.com>
2025-03-04 11:14:04 +0530

    Update Comments.jsx

--- frontend/src/components/Comments.jsx
+++ frontend/src/components/Comments.jsx
@@ -1,6 +1,12 @@
 import axios from "axios";
 import Comment from "./Comment";
-import { useQuery } from "@tanstack/react-query";
+import {
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { useAuth } from "@clerk/clerk-react";
+import { toast } from "react-toastify";
 
 const fetchComments = async (postId) => {
   const res = await axios.get(
@@ -11,22 +17,60 @@ const fetchComments = async (postId) => {
 
 // eslint-disable-next-line react/prop-types
 const Comments = ({ postId }) => {
+  const { getToken } = useAuth();
+
   const { isPending, error, data } = useQuery({
     queryKey: ["comments", postId],
     queryFn: () => fetchComments(postId),
+  });
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: async (newComment) => {
+      const token = await getToken();
+      return axios.post(
+        `${import.meta.env.VITE_API_URL}/comments/${postId}`,
+        newComment,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["comments", postId] });
+    },
+    onError: (error) => {
+      toast.error(error.response.data);
+    },
   });
 
   if (isPending) return "Loading...";
   if (error) return "Something went wrong!" + error.message;
   if (!data) return "Comments not found";
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const data = {
+      desc: formData.get("desc"),
+    };
+    mutation.mutate(data);
+  };
+
   return (
     <>
       <div className="flex flex-col gap-8 lg:w-3/5 mt-5">
         <h1 className="text-xl text-gray-500 underline">Comments</h1>
-        <div className="flex items-center justify-between gap-8 w-full">
+        <form
+          onSubmit={handleSubmit}
+          className="flex items-center justify-between gap-8 w-full"
+        >
           <textarea
-            name=""
+            name="desc"
             id=""
             placeholder="Write a comment..."
             className="p-5 w-full"
@@ -34,7 +78,7 @@ const Comments = ({ postId }) => {
           <button className="border-2 p-2 px-5 bg-blue-800 font-medium text-white">
             Comment
           </button>
-        </div>
+        </form>
         {data.map((comment) => (
           <Comment key={comment._id} comment={comment} />
         ))}
